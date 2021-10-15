f4eb821892455e2669af6866835c2943960e5460
Hashan Eranga <ccmrhe@gmail.com>
2021-10-16 00:13:32 +0530

    validations():using express navigator

--- routes/userApi.js
+++ routes/userApi.js
@@ -6,9 +6,18 @@ router.get("/", (req, res)=>{
     res.send("Users Route")
 })
 
-router.post("/", (req, res)=>{
-    console.log(req.body)
-    res.send("Users Route")
+router.post("/", [
+    check('name', 'Name is required').not().isEmpty(), 
+    check('email', 'Please Enter a valid Email address').isEmail(),
+    check('password', 'Please your password should have 5 characters').isLength({min:5})
+    ],
+    (req, res)=>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+        res.status(200).send('Users Route')
+
 })
 
 module.exports = router
